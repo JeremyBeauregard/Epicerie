81804c9198fb18d1c2503a8e337e31d1773c065f
Jeremy Beauregard <jer.beauregard@gmail.com>
2016-02-13 09:42:15 -0500

    Défini la classe Nourriture

--- app/src/main/java/com/uqac/informatiquemobile/epicerie/Nourriture.java
+++ app/src/main/java/com/uqac/informatiquemobile/epicerie/Nourriture.java
@@ -3,5 +3,21 @@ package com.uqac.informatiquemobile.epicerie;
 /**
  * Created by Jeremy on 2016-02-13.
  */
-public class Nourriture {
+public abstract class Nourriture {
+
+    private String _nom;
+
+    // Constructeur
+    public void Nourriture(String nom) {
+        setNom(nom);
+    }
+
+
+    // Accesseurs
+    public String getNom() {return _nom;}
+    private void setNom(String value) {_nom=value;}
+    public abstract Float getPrix();
+
+
+
 }
