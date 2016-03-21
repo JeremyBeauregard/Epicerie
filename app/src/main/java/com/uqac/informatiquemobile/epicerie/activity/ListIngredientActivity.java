315b0a9e5ee69fce3985795b946aee71198f653c
paull <paul.lesur@free.fr>
2016-03-21 17:09:11 -0400

    Refactor

--- app/src/main/java/com/uqac/informatiquemobile/epicerie/activity/ListIngredientActivity.java
+++ app/src/main/java/com/uqac/informatiquemobile/epicerie/activity/ListIngredientActivity.java
@@ -21,7 +21,7 @@ public class ListIngredientActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_ingredients_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
