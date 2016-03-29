182dfeee90d006a83e12e749e63b15434c9dab67
paull <paul.lesur@free.fr>
2016-03-29 18:13:24 -0400

    Ajout d'un menu

---
+++ app/src/main/java/com/uqac/informatiquemobile/epicerie/activity/MainMenu.java
@@ -0,0 +1,34 @@
+package com.uqac.informatiquemobile.epicerie.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.uqac.informatiquemobile.epicerie.R;
+
+/**
+ * Created by paull on 29/03/2016.
+ */
+public class MainMenu extends AppCompatActivity {
+
+    private Button buttonProvisions, buttonCourses, buttonCalendrier, buttonRecettes;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_menu_activity);
+        buttonProvisions = (Button)findViewById(R.id.buttonFrigo);
+        buttonRecettes= (Button)findViewById(R.id.buttonRecettes);
+        buttonCalendrier= (Button)findViewById(R.id.buttonCalendrier);
+        buttonCourses= (Button)findViewById(R.id.buttonCourses);
+
+        buttonRecettes.setEnabled(false);
+        buttonCalendrier.setEnabled(false);
+        buttonCourses.setEnabled(false);
+
+
+
+    }
+}
