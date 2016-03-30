7954e08cd72d5871bd9cc06c4704902f2a5842bb
Guilotz <guilotz@hotmail.fr>
2016-03-30 07:37:05 -0400

    started list recettes activity

--- app/src/main/java/com/uqac/informatiquemobile/epicerie/activity/ListRecettesActivity.java
+++ app/src/main/java/com/uqac/informatiquemobile/epicerie/activity/ListRecettesActivity.java
@@ -1,7 +1,13 @@
 package com.uqac.informatiquemobile.epicerie.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.uqac.informatiquemobile.epicerie.R;
 
 /**
  * Created by Guillaume2 on 30/03/2016.
@@ -11,6 +17,20 @@ public class ListRecettesActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_recettes_activity);
+        
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(), CreateIngredientActivity.class);
+                startActivityForResult(i, 123);
+            }
+        });
 
     }
 }
