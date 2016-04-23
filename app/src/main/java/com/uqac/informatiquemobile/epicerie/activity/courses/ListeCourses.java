2171b733d5ed9dcd70ac35978c007de8bde89dfb
babisouk <franco.cantin@gmail.com>
2016-04-23 14:21:24 -0400

    Changé les couleurs dans ListeCourseActivity, Hormis la list view.

--- app/src/main/java/com/uqac/informatiquemobile/epicerie/activity/courses/ListeCourses.java
+++ app/src/main/java/com/uqac/informatiquemobile/epicerie/activity/courses/ListeCourses.java
@@ -2,6 +2,7 @@ package com.uqac.informatiquemobile.epicerie.activity.courses;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
